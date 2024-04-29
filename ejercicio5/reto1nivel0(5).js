106bcd0c457baf7e77e531aa04278553dd3de6f1
Markos-19 <gonzalez.marcos19@gmail.com>
2024-04-29 13:05:42 -0300

    se finaliza el ejercicio 5

--- ejercicio5/reto1nivel0(5).js
+++ ejercicio5/reto1nivel0(5).js
@@ -1,6 +1,6 @@
 let informacionFinanciera = {
     ingresos: 0,
-    gastos:{
+    gastos: {
         hogar: [],
         medicos: [],
         ocio: [],
@@ -13,39 +13,55 @@ function nuevoIngreso  (monto) {
      informacionFinanciera.ingresos += monto;
      return informacionFinanciera.ingresos;
     }
-console.log(nuevoIngreso(500))
+//console.log(nuevoIngreso(500))
+//console.log(nuevoIngreso(500))
 
-function registrarGastos (category, monto) {
-   
-    informacionFinanciera.gastos[category] = (informacionFinanciera.gastos[category] || 0);
-    informacionFinanciera.gastos[category] += monto;    
+function registrarGastos(category, monto) {
+    informacionFinanciera.gastos[category] =
+      Number(informacionFinanciera.gastos[category]) || 0;
+    informacionFinanciera.gastos[category] += monto;
     return informacionFinanciera.gastos[category];
-};
-console.log(registrarGastos("ocio", 500))
-console.log(registrarGastos("ocio", 500))
+  }
+//console.log(registrarGastos("ocio", 500))
+//console.log(registrarGastos("ocio", 500))
+//console.log(registrarGastos("ahorro", 500))
+//console.log(registrarGastos("ahorro", 1500))
 
 function gastosCategorias (){
     let gastosCategorias = 0;
     for (let category in informacionFinanciera.gastos){
-        gastosCategorias += informacionFinanciera.gastos.category;
+        gastosCategorias += Number (informacionFinanciera.gastos[category]);
     } return gastosCategorias
 };
 
-console.log(gastosCategorias)
+//console.log(gastosCategorias())
 
 function chequeoEstadoFinanciero () {
     let totalIngresos = informacionFinanciera.ingresos;
     let totalGastos = gastosCategorias();
+    let categoriaMasGastos = '';
+    let maxGastos = 0;
 
+    for (let category in informacionFinanciera.gastos) {
+        if (informacionFinanciera.gastos.hasOwnProperty(category)) {
+            if (informacionFinanciera.gastos[category] > maxGastos) {
+                maxGastos = informacionFinanciera.gastos[category];
+                categoriaMasGastos = category;
+            }
+        }
+    }        
         if (totalGastos === totalIngresos) {
-            alert ('SUS INGRESOS HAN SIDO GASTADOS. POR FAVOR REDUZCA SUS GASTOS.');
+            return (`SUS INGRESOS HAN SIDO GASTADOS. POR FAVOR REDUZCA SUS GASTOS.`);
         }else if(totalGastos < totalIngresos){
-            alert ('FELICITACIONES!! HA GASTADO MENOS QUE SUS INGRESOS.');
+            return (`FELICITACIONES!! HA GASTADO MENOS QUE SUS INGRESOS.`);
         } else {
-            alert ('DEBERIA INTENTAR REDUCIR SUS GASTOS EN'[categorias]);
+            return (`DEBERIA INTENTAR REDUCIR SUS GASTOS EN ${categoriaMasGastos}`);
         }
     }
-console.log(nuevoIngreso(2000));
+/*console.log(nuevoIngreso(6000));
+console.log(nuevoIngreso(1000));
+console.log(nuevoIngreso(2000))
 console.log(registrarGastos("medicos", 500));
-console.log(registrarGastos("ocio", 500));
-console.log(registrarGastos("ahorro", 500))
+console.log(registrarGastos("ocio", 2500));
+console.log(gastosCategorias(""))
+console.log(chequeoEstadoFinanciero())*/
